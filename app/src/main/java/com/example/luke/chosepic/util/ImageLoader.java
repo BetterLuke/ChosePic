be185faca008b1fb8fc8572f78562c1e0211c109
LUKE <1147357044@qq.com>
2015-10-06 00:49:37 +0800

    +后台轮询线程的并行 +利用信号量控制任务队列采用LIFO策略 +完善关键代码的注释

--- app/src/main/java/com/example/luke/chosepic/util/ImageLoader.java
+++ app/src/main/java/com/example/luke/chosepic/util/ImageLoader.java
@@ -1,6 +1,7 @@
 package com.example.luke.chosepic.util;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Looper;
 import android.os.Message;
 import android.util.DisplayMetrics;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 /**
  * 图片加载类
@@ -49,6 +51,20 @@ public class ImageLoader {
      * UI线程中的Handler
      */
     private android.os.Handler mUIHandler;
+
+    /**
+     * 后台轮询线程的信号量，并设置许可数为0
+     */
+    private Semaphore mSemaphorePoolThreadHandler = new Semaphore(0);
+
+    /**
+     * 线程池的信号量，许可值在init（）类初始化
+     */
+    private Semaphore mSemaphoreThreadPool;
+
+    /**
+     * 自定义的两种枚举类型
+     */
     public enum Type{
         FIFO,LIFO;
     }
@@ -61,22 +77,32 @@ public class ImageLoader {
 
     /**
      * 初始化ImageLoader
-     * @param threadCount
-     * @param type
+     * @param threadCount 线程数
+     * @param type 加载策略
      */
     private void init(int threadCount, Type type) {
         //后台轮询线程
         mPoolThread = new Thread(){
             @Override
             public void run() {
+
                 Looper.prepare();
+
                 mPoolThreadHandler = new android.os.Handler(){
                     @Override
                     public void handleMessage(Message msg) {
                         //线程池去取出一个任务进行执行
                         mThreadPool.execute(getTask());
+                        try {
+                            mSemaphoreThreadPool.acquire();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 };
+                //mPoolThreadHandler完成初始化，已不为null，即释放一个信号量，增加了一个许可
+                mSemaphorePoolThreadHandler.release();
+
                 Looper.loop();
             }
         };
@@ -94,8 +120,13 @@ public class ImageLoader {
 
         //创建线程池
         mThreadPool = Executors.newFixedThreadPool(threadCount);
+        //创建任务队列
         mTaskQueue = new LinkedList<Runnable>();
+
         mType = type;
+
+        //初始化线程池信号量的许可值
+        mSemaphoreThreadPool = new Semaphore(threadCount);
     }
 
     /**
@@ -131,7 +162,7 @@ public class ImageLoader {
      * @param path
      * @param imageView
      */
-    public void loadImage(String path, final ImageView imageView){
+    public void loadImage(final String path, final ImageView imageView){
         imageView.setTag(path);
         if (mUIHandler == null){
             mUIHandler = new android.os.Handler(){
@@ -150,29 +181,95 @@ public class ImageLoader {
             };
         }
 
-        //根据Path在缓存中获取bitmap
+        //根据Path在缓存中获取对应的bitmap
         Bitmap bm = getBitmapFromLruCache(path);
 
-        //新建message，内含有holder对象，发送message给mUIHolder
+        //加载图片的显示
         if (bm != null){
-            Message message = Message.obtain();
-            ImgBeanHolder holder = new ImgBeanHolder();
-            holder.path = path;
-            holder.bitmap = bm;
-            holder.imageView = imageView;
-            message.obj = holder;
-            mUIHandler.sendMessage(message);
+            refreshBitmap(path, imageView, bm);
         }else{
+            //向任务队列增加一个任务
             addTask(new Runnable(){
                 @Override
                 public void run() {
-                    //加载图片
-                    //图片的压缩: 1.获得图片需要显示的大小
-                    //
+                    //任务是要加载图片，就要先对图片进行压缩
+                    //图片的压缩:
+                    //   1.获得图片需要显示的大小
                    ImageSize imageSize = getImageViewSize(imageView);
+                    //   2.压缩图片
+                    Bitmap bm = decodeSampledBitmapFromPath(path,imageSize.width,imageSize.height);
+                    //   3.把图片加入到缓存
+                    addBitmapToLruCache(path,bm);
+                    refreshBitmap(path,imageView,bm);
+
+                    mSemaphoreThreadPool.release();//每增加一个任务，就释放一个信号量
                 }
             });
         }
+    }
+
+    /**
+     * 加载图片的显示
+     * @param path
+     * @param imageView
+     * @param bm
+     */
+    private void refreshBitmap(String path, ImageView imageView, Bitmap bm) {
+        Message message = Message.obtain();
+        ImgBeanHolder holder = new ImgBeanHolder();
+        holder.path = path;
+        holder.bitmap = bm;
+        holder.imageView = imageView;
+        message.obj = holder;
+        mUIHandler.sendMessage(message);
+    }
+
+    /**
+     * 将图片加入LruCache
+     * @param path
+     * @param bm
+     */
+    private void addBitmapToLruCache(String path, Bitmap bm) {
+        if (getBitmapFromLruCache(path) == null){
+            mLruCache.put(path,bm);
+        }
+    }
+
+    private Bitmap decodeSampledBitmapFromPath(String path, int width, int height) {
+        //获得图片的宽和高,并不把图片加载到内存中
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path,options);
+
+        options.inSampleSize = caculateInSampleSize(options,width,height);
+
+        //使用获得到的InSampleSize再次解析图片
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+
+        return bitmap;
+    }
+
+    /**
+     * 根据需求的宽和高以及图片实际的宽和高计算SampleSize
+     * @param options
+     * @param reqwidth
+     * @param reqheight
+     * @return
+     */
+    private int caculateInSampleSize(BitmapFactory.Options options, int reqwidth, int reqheight) {
+        int width = options.outWidth;
+        int height = options.outHeight;
+
+        int inSampleSize = 1;
+
+        if (width > reqwidth || height > reqheight){
+            int widthRadio = Math.round(width * 1.0f / reqwidth);
+            int heightRadio = Math.round(height * 1.0f / reqheight);
+
+            inSampleSize = Math.min(widthRadio,heightRadio);//采用min()是想保持比例失真率最小
+        }
+        return inSampleSize;
     }
 
     /**
@@ -221,9 +318,21 @@ public class ImageLoader {
         int height;
     }
 
-    //向TaskQueue里添加一个Task
-    private void addTask(Runnable runnable) {
-        mTaskQueue.add(runnable);
+    //向TaskQueue里添加一个Task,加同步
+    private synchronized void addTask(Runnable runnable) {
+        mTaskQueue.add(runnable);//向线程池中增加Task,也是要addTask()方法声明synchronized同步的原因
+
+        //此处若mPoolThreadHandler没有完成初始化，线程可能会发生异常（空指针），所以需要确保
+        //mPoolThreadHandler在调用前不为null,这里使用线程信号量机制控制mPoolThreadHandler的初始化
+        //在被调用前执行,loadImage()方法内有对addTask()方法的调用
+
+        try {
+            if (mPoolThreadHandler == null) {
+                mSemaphorePoolThreadHandler.acquire();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         mPoolThreadHandler.sendEmptyMessage(0x110);
     }
 
